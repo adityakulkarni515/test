a0815dfe9ec7afdb01a1260f98dcc6ae0e6f12af
Aditya Kulkarni <adityakulkarni515@Adityas-MacBook-Air.local>
2025-05-16 07:38:17 +0530

    added in test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-def greet(name):
+def test_greet(name):
     print(f"Hello, {name}! Welcome to Python.")
 
 # Run the function
 if __name__ == "__main__":
-    greet("Aditya")
+    test_greet("Aditya")
 
